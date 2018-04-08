a4ff5e9d97f2fbcb0a67eb5abd272c3a2d6bc591
Shaun McArthur <shamca65@gmail.com>
2018-04-08 22:44:07 +0000

    Handle exceptions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  protected
   
+  def resource_not_found
+    message = "The article you are looking for does not exist"
+    flash[:alert] = message
+    redirect_to root_path
+
+  end
+
   private 
   
   def article_params
